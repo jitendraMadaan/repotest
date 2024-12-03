6b6c1cf6c959df9da2e8391aad433faadc422c10
jitendra madaan <jitendramadaan47@yahoo.com>
2024-12-03 18:23:42 +0530

    updated today 1

--- gitTest/src/pack/hello.java
+++ gitTest/src/pack/hello.java
@@ -5,7 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("hello!!!!");
+		System.out.print("hello updated new!!!!");
 
 	}
 
